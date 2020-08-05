528881f6ab3735c856758ab84143bf8b1d34afce
Jai Gupta <jaigupta1998@gmail.com>
2020-08-05 15:08:56 +0530

    added div module

--- EmployeeManagement.java
+++ EmployeeManagement.java
@@ -5,4 +5,7 @@ class  Calculator{
   public void diff(int a, int b){
     System.out.println("Diff = "+(a-b));
   }
+  public void div(int a, int b){
+    System.out.println("Quotient = "+(a/b));
+  }
 }
